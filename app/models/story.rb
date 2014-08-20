5ca4d40b9d747cd3e780f2bb2bfd417ff1569e47
Håvard Magne Fagervoll <fagervoll@gmail.com>
2014-08-20 19:51:36 +0200

    Fix last change into models and migrations

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,7 @@
 class Continent < ActiveRecord::Base
 	belongs_to :continent
 	belongs_to :country
+	belongs_to :happened_in, class_name: "Country"
 	  
 	validates :story, length: { maximum: 2000 }
 end
--- db/migrate/20140815124625_create_stories.rb
+++ db/migrate/20140815124625_create_stories.rb
@@ -1,11 +1,12 @@
 class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
-      t.string :title
+      t.string :author
       t.text :story
       t.references :continent, index: true
-      t.references :country, index: true
-      t.string :mail
+      t.references :author_nationality, index: true
+      t.references :happened_in, index: true
+      t.string :email
       t.integer :rating
 
       t.timestamps
