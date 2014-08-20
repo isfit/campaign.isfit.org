ad7dc1f3b1169009ca62a3d3da8aa344d58c5d48
Håvard Magne Fagervoll <fagervoll@gmail.com>
2014-08-20 19:59:40 +0200

    Adds infopage and important update to story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Continent < ActiveRecord::Base
 	belongs_to :continent
-	belongs_to :country
+	belongs_to :author_nationality, class_name: "Country"
 	belongs_to :happened_in, class_name: "Country"
 	  
 	validates :story, length: { maximum: 2000 }
