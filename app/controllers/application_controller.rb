8670b86758ec554563fb591c26e4112a374c0ff9
Håvard Magne Fagervoll <fagervoll@gmail.com>
2014-09-08 18:20:24 +0200

    Adds coming soon before release

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :released?
+
+  def released?
+  	release = Time.now > Time.parse('2014-09-31 18:00')
+
+  	if !release
+  		render text: 'Coming soon' and return
+  	end
+  end
 end
