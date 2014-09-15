ce42b7c1a7ab60896287ee1383452e6db1d9f712
andreawi <andrewis@stud.ntnu.no>
2014-09-15 21:56:05 +0200

    No default country in dropdown menu, but not working correctly when left empty. No error messages come up

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,9 +4,9 @@ class StoriesController < ApplicationController
 
 		# Bør gjøres på en bedre måte
 		p = story_params
-		p[:happened_in] = Country.find(p[:happened_in].to_i)
-		p[:author_nationality] = Country.find(p[:author_nationality].to_i)
-		p[:continent] = Continent.find(p[:continent].to_i)
+		#p[:happened_in] = Country.find(p[:happened_in].to_i)
+		#p[:author_nationality] = Country.find(p[:author_nationality].to_i)
+		#p[:continent] = Continent.find(p[:continent].to_i)
 
 		@story = Story.new(p)
 
@@ -42,7 +42,7 @@ class StoriesController < ApplicationController
 
 	private
 		def story_params
-			params.require(:story).permit(:happened_in, :story, :author, :author_nationality, :email, :continent, :title)
+			params.require(:story).permit(:happened_in_id, :story, :author, :author_nationality_id, :email, :continent_id, :title)
 		end
 
 end
