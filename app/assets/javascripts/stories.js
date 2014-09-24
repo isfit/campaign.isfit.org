956ee565bce09a5a27760ec0a3e8ac72050fc9fa
andreawi <andrewis@stud.ntnu.no>
2014-09-24 20:23:55 +0200

    Added facebook and twitter share button on stories

---
+++ app/assets/javascripts/stories.js
@@ -0,0 +1,36 @@
+var ready;
+ready = function() {
+    $.getJSON("http://graph.facebook.com/?id=" + window.location.href, function (json){
+        if(typeof json.shares === 'undefined'){
+          $('#fb-counter1').text(0);
+          $('#fb-counter2').text(0);
+        }
+        else{
+          $('#fb-counter1').text(json.shares);
+          $('#fb-counter2').text(json.shares);
+        }
+    });
+    function getTwitterCount(url) {
+        $.ajax({
+            method: "GET",
+            url: url,
+            dataType: "jsonp",
+            jsonp: "callback",
+            success: function (json) {
+                if(typeof json.count === 'undefined'){
+                    $('#twitter-counter1').text(0);
+                    $('#twitter-counter2').text(0);
+                }
+                else{
+                    $('#twitter-counter1').text(json.count);
+                    $('#twitter-counter2').text(json.count);
+                }
+            }
+        });
+    }
+    getTwitterCount("http://urls.api.twitter.com/1/urls/count.json?url=" + window.location.href);
+
+    
+};
+$(document).ready(ready);
+$(document).on('page:load', ready);
