405c5529e29da4013fef362f09e93124c21b2f18
Håvard Magne Fagervoll <fagervoll@gmail.com>
2014-10-06 22:38:32 +0200

    Fix twitter and facebook share in footer

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -10,6 +10,14 @@ ready = function() {
           $('#fb-counter2').text(json.shares);
         }
     });
+    $.getJSON("http://graph.facebook.com/?id=http://www.theglobalinfection.com", function (json){
+        if(typeof json.shares === 'undefined'){
+          $('#fb-counter-footer').text(0);
+        }
+        else{
+          $('#fb-counter-footer').text(json.shares);
+        }
+    });
     function getTwitterCount(url) {
         $.ajax({
             method: "GET",
@@ -19,16 +27,33 @@ ready = function() {
             success: function (json) {
                 if(typeof json.count === 'undefined'){
                     $('#twitter-counter1').text(0);
-                    $('#twitter-counter2').text(0);
                 }
                 else{
                     $('#twitter-counter1').text(json.count);
-                    $('#twitter-counter2').text(json.count);
                 }
             }
         });
     }
+    function getTwitterCountFooter(url) {
+        $.ajax({
+            method: "GET",
+            url: url,
+            dataType: "jsonp",
+            jsonp: "callback",
+            success: function (json) {
+                if(typeof json.count === 'undefined'){
+                    $('#twitter-counter-footer').text(0);
+                }
+                else{
+                    $('#twitter-counter-footer').text(json.count);
+                }
+            }
+        });
+    }
+
     getTwitterCount("http://urls.api.twitter.com/1/urls/count.json?url=" + window.location.href);
+    getTwitterCountFooter("http://urls.api.twitter.com/1/urls/count.json?url=http://www.theglobalinfection.com");
+
 
     
 };
